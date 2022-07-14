dd2f413cc54731480077f7c166d62183edddbf1f
aba32824@slack.launchschool.com <okikukai.kiev@gmail.com>
2022-07-15 00:59:56 +0300

    Asking user to continue the game or not

--- lesson_2/rps_game.js
+++ lesson_2/rps_game.js
@@ -15,14 +15,25 @@ function getRandomItem() {
   return VALID_CHOICES[index];
 }
 
-prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-let choice = readline.question();
+while (true) {
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  let choice = readline.question();
 
-while (!VALID_CHOICES.includes(choice)) {
-  prompt("That's not a valid choice. Please repeat");
-  choice = readline.question();
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt("That's not a valid choice. Please repeat");
+    choice = readline.question();
+  }
+
+  let computerChoice = getRandomItem();
+
+  prompt(`Your chose ${choice}, computer chose ${computerChoice}`);
+  // Do something to detect a winner
+  prompt('Do you want to play again (y/n)?');
+  let answer = readline.question().toLowerCase();
+  while (answer !== 'y' && answer !== 'n') {
+    prompt('Please input either "y" or "n"');
+    answer = readline.question().toLowerCase();
+  }
+
+  if (answer == 'n') break;
 }
-
-let computerChoice = getRandomItem();
-
-prompt(`Your chose ${choice}, computer chose ${computerChoice}`);
