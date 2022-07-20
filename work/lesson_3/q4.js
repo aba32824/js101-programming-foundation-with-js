f0712e36b9be5cdbd9be679147c0aadbece6d2be
aba32824@slack.launchschool.com <okikukai.kiev@gmail.com>
2022-07-20 23:36:03 +0300

    Adding lesson_3 4th question

---
+++ work/lesson_3/q4.js
@@ -0,0 +1,13 @@
+// Q: Using the following string, create a new string that 
+//    contains all lowercase letters except for the first character,
+//    which should be capitalized.
+
+let munstersDescription = "the Munsters are CREEPY and Spooky.";
+// => The munsters are creepy and spooky.
+
+// A: one way to do so could be done via a combination of the following methods:
+//    - String.prototype.toUpperCase()[0] - to capitalize the first character
+//    - String.prototype.toLowerCase().slice(1,) - to "lowercase" the rest of chars
+//    - and then concatenate all the above
+
+let desc = munstersDescription[0].toUpperCase() + munstersDescription.toLocaleLowerCase().slice(1,);
