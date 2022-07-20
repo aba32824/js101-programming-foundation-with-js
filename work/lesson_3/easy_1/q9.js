eda465dc1973481fc88c94e9571401d6c6b825ce
aba32824@slack.launchschool.com <okikukai.kiev@gmail.com>
2022-07-20 23:58:27 +0300

    Adding 9th question

---
+++ work/lesson_3/easy_1/q9.js
@@ -0,0 +1,8 @@
+// In the previous problem, our first answer added 'Dino' to the array like this:
+let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
+flintstones.push("Dino");
+
+// Q: How can we add multiple items to our array? ('Dino' and 'Hoppy')
+// A: You can add multiple items via Array.prototype.push
+flintstones.push("Dino", "Hoppy");
+
