d005682a4551fa16c1e5ab7f8f290dfc6872737a
aba32824@slack.launchschool.com <okikukai.kiev@gmail.com>
2022-07-20 23:39:40 +0300

    Adding the 5th question + amending the 4th

---
+++ work/lesson_3/easy_1/q5.js
@@ -0,0 +1,7 @@
+// Q: What will the following code output?
+
+console.log(false == '0');
+console.log(false === '0');
+
+// A: the first statement will output 'true'
+//    and the second one 'false'
