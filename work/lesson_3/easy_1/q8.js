08dc2a48398cd9befc584606ba0b010456d85074
aba32824@slack.launchschool.com <okikukai.kiev@gmail.com>
2022-07-20 23:55:38 +0300

    Adding 8th question

---
+++ work/lesson_3/easy_1/q8.js
@@ -0,0 +1,5 @@
+// Q: How can we add the family pet, "Dino", to the following array?
+let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
+
+// A: it's possible to do via Array.prototype.push method
+flintstones.push('Dino');
