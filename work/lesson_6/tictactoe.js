4ca738714b9954467fe966a03544ac8ad4d62874
aba32824@slack.launchschool.com <okikukai.kiev@gmail.com>
2022-08-05 19:58:21 +0300

    [LESSON 6] Adding a draft tic-tac-toe game app.

---
+++ work/lesson_6/tictactoe.js
@@ -0,0 +1,116 @@
+/**
+ * 
+ * Tic Tac Toe is a 2-player game played on a 3x3 grid called the board.
+ * Each player takes a turn and marks a square on the board. The first player
+ * to get 3 squares in a row–horizontal, vertical, or diagonal–wins.
+ * If all 9 squares are filled and neither player has 3 in a row,
+ * the game is a tie.
+ * 
+ */
+
+const readline = require('readline-sync');
+
+// Board and cell (AKA square) settings
+const BOARD = {
+  rowA: { rowId: 'A', cells: [] },
+  rowB: { rowId: 'B', cells: [] },
+  rowC: { rowId: 'C', cells: [] }
+};
+const CELL = {
+  hasMark: false,
+  mark: undefined,
+  player: null,
+  cellId: 0
+};
+
+// Players
+const PC_PLAYER = {
+  name: "computer",
+  mark: "X"
+};
+const HUMAN_PLAYER = {
+  name: "you",
+  mark: "0"
+};
+
+function initBoard() {
+  Object.keys(BOARD).forEach(row => {
+    for (let i = 1; i <= 3; i++) {
+      let cell = JSON.parse(JSON.stringify(CELL));
+      cell.cellId = i;
+      BOARD[row]['cells'].push(cell);
+    }
+  });
+}
+
+function resetBoard() {
+  Object.keys(BOARD).forEach(row => {
+    BOARD[row]['cells'].length = 0;
+  });
+}
+
+function displayEmptyBoard() {
+  let rowBorder = '+' + '-'.repeat(3);
+  let rowSection = '|' + ' '.repeat(3);
+
+  // getting Board's row and cell IDs to  display them to the player
+  let rowIds = Object.values(BOARD).map(row => row.rowId);
+  let cellIds = Array.from({length: Object.keys(BOARD).length}, (v, i) => i + 1);
+  // just an empty line
+  console.log();
+  // to display cell IDs
+  console.log(' '.repeat(5) + cellIds.join(' | '));
+  // to display the empty board and row IDs
+  for (let i = 0; i < 3; i++) {
+    console.log(' '.repeat(3) + rowBorder.repeat(3) + '+');
+    console.log(' ' + rowIds[i] + ' ' + rowSection.repeat(3) + '|');
+  }
+  console.log(' '.repeat(3) + rowBorder.repeat(3) + '+');
+}
+
+function displayWinner() {
+  return null;
+}
+
+function letUserMarkBoard() {
+  return null;
+}
+
+function letComputerMarkBoard() {
+  return null;
+}
+
+function displayGameRules() {
+  console.log('*'.repeat(29) + " Tic Tac Toe " + '*'.repeat(29));
+  console.log("It's a 2-player game played on a 3x3 grid called the board.");
+  console.log("Each player takes a turn and marks a square on the board.");
+  console.log("A player wins if he/she gets 3 squares in:");
+  console.log(" - row–horizontal\n - vertical\n - or diagonal");
+  console.log("It's a tie if all 9 squares are filled and none has 3 in a row.");
+  console.log("To input your mark you have specify a row Id and cell Id.");
+  console.log("Row IDs are labeled as A, B or C, and cell IDs - 1, 2 and 3");
+  console.log('*'.repeat(70));
+}
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function getHumanPlayerRowIdInput(text) {
+  prompt(text);
+  let rowId;
+  while (true) {
+    rowId = readline.question('> ').toUpperCase();
+    if (Object.values(BOARD).map(row => row.rowId).includes(rowId)) break;
+    prompt('[WARN] Please input a valid row ID!');
+  }
+  return rowId;
+}
+
+displayGameRules();
+// Main loop
+while (true) {
+  displayEmptyBoard();
+  let rowId = getHumanPlayerRowIdInput('Please specify row ID')
+  break;
+}
